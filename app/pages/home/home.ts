fdcdd0b0caee41a442e2e3e7a8d74cdc55fbdb92
Anjali Garg <anjali@headerlabs.com>
2016-09-03 13:18:43 +0530

    format changed

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,29 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController ,Platform} from 'ionic-angular';
+import { NavController, Platform} from 'ionic-angular';
 import { SocialSharing } from 'ionic-native';
 
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-   constructor(private platform: Platform) {
+    constructor(private platform: Platform) {
         this.platform = platform;
     }
- 
+
     share(message, subject, file, link) {
         this.platform.ready().then(() => {
             SocialSharing.share(message, subject, file, link);
         });
     }
- 
+
     shareViaEmail(message, image, link) {
         this.platform.ready().then(() => {
             SocialSharing.canShareViaEmail().then(() => {
-                SocialSharing.shareViaEmail(message,image,link);
-            },(err)=>{
+                SocialSharing.shareViaEmail(message, image, link);
+            }, (err) => {
                 console.error(err);
             });
         });
     }
+    shareViaFacebook(message, image, url) {
+        this.platform.ready().then(() => {
+            SocialSharing.shareViaFacebook(message, image, url);
+
+        });
+    }
+    shareViaWhatsApp(message, image, url) {
+        this.platform.ready().then(() => {
+            SocialSharing.shareViaWhatsApp(message, image, url);
+        });
+    }
+
 }
